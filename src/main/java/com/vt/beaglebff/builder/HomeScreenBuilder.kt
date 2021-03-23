591cb4e58b585c4297fd5c28a1ce5e5b3c84d713
hoangnv65 <hoangnv65@viettel.com.vn>
2021-03-23 13:43:42 +0700

    add

--- src/main/java/com/vt/beaglebff/builder/HomeScreenBuilder.kt
+++ src/main/java/com/vt/beaglebff/builder/HomeScreenBuilder.kt
@@ -3,10 +3,16 @@ package com.vt.beaglebff.builder
 import br.com.zup.beagle.core.*
 import br.com.zup.beagle.ext.*
 import br.com.zup.beagle.widget.context.ContextData
+import br.com.zup.beagle.widget.context.expressionOf
 import br.com.zup.beagle.widget.core.*
 import br.com.zup.beagle.widget.layout.*
+import br.com.zup.beagle.widget.navigation.Touchable
 import br.com.zup.beagle.widget.pager.PageIndicator
 import br.com.zup.beagle.widget.ui.*
+import com.vt.beaglebff.components.actions.ToastAction
+import br.com.zup.beagle.widget.core.JustifyContent
+import com.vt.beaglebff.components.widgets.ItemRowDivider
+
 
 class HomeScreenBuilder : ScreenBuilder {
     override fun build(): Screen {
@@ -16,6 +22,12 @@ class HomeScreenBuilder : ScreenBuilder {
                                 createToolbar(),
                                 createBannerView(),
                                 createBottomNavigationBar()
+                        )
+                ).applyStyle(
+                        Style(
+                                flex = Flex(
+                                        grow = 1.0
+                                )
                         )
                 )
         )
@@ -46,7 +58,10 @@ class HomeScreenBuilder : ScreenBuilder {
                     size = Size(width = 100.unitPercent(), height = 50.unitPercent())
             )
     ).applyFlex(
-            Flex(alignSelf = AlignSelf.CENTER, justifyContent = JustifyContent.FLEX_START)
+            Flex(
+                    alignSelf = AlignSelf.CENTER,
+                    justifyContent = JustifyContent.FLEX_START
+            )
     )
 
     private fun createToolbar() = Container(
@@ -54,13 +69,17 @@ class HomeScreenBuilder : ScreenBuilder {
                     Text("")
             )
     ).applyStyle(
-            Style(backgroundColor = "#0F8E70",
+            Style(backgroundColor = "#3596EC",
                     size = Size(width = 100.unitPercent(), height = 56.unitReal()),
                     position = EdgeValue(0.unitReal()),
-                    positionType = PositionType.RELATIVE)
+                    positionType = PositionType.RELATIVE
+            )
 
     ).applyFlex(
-            Flex(alignSelf = AlignSelf.CENTER, justifyContent = JustifyContent.FLEX_START)
+            Flex(
+                    alignSelf = AlignSelf.CENTER,
+                    justifyContent = JustifyContent.FLEX_START
+            )
     ).setId("widget")
 
     private fun createBannerImage(remoteUrl: String) = Image(
@@ -75,18 +94,92 @@ class HomeScreenBuilder : ScreenBuilder {
 
     private fun createBottomNavigationBar() = Container(
             children = listOf(
-                    TabView(children =
-                    listOf(
-                            TabItem("Tab 1",
-                                    Text("First Tab Content").applyFlex(flex = Flex(grow = 1.0))
-                            ),
-                            TabItem("Tab 2",
-                                    Text("Second Tab Content").applyFlex(flex = Flex(grow = 1.0))
+                    ItemRowDivider(
+                            expressionOf("#B9B9B9"),
+                            expressionOf("1"),
+                            leftMargin = 0,
+                            rightMargin = 0,
+                            topMargin = 0,
+                            bottomMargin = 0
+                    ),
+                    Container(
+                            children = listOf(
+                                    createBottomButton(
+                                            text = "Home",
+                                            remoteIconUrl = "http://10.0.2.2:8080/resourcesController/ic_account_info"
+                                    ),
+                                    createBottomButton(
+                                            text = "Tasks",
+                                            remoteIconUrl = "http://10.0.2.2:8080/resourcesController/ic_account_info"
+                                    ),
+                                    createBottomButton(
+                                            text = "Requests",
+                                            remoteIconUrl = "http://10.0.2.2:8080/resourcesController/ic_account_info"
+                                    ),
+                                    createBottomButton(
+                                            text = "Chat",
+                                            remoteIconUrl = "http://10.0.2.2:8080/resourcesController/ic_account_info"
+                                    ),
+                                    createBottomButton(
+                                            text = "Notifications",
+                                            remoteIconUrl = "http://10.0.2.2:8080/resourcesController/ic_account_info"
+                                    )
                             )
-                    )
+                    ).applyFlex(
+                            Flex(
+                                    flexDirection = FlexDirection.ROW,
+                                    flexWrap = FlexWrap.NO_WRAP
+                            )
                     )
             )
     ).applyFlex(
-            Flex(alignSelf = AlignSelf.CENTER, justifyContent = JustifyContent.FLEX_END)
+            Flex(
+                    flexDirection = FlexDirection.COLUMN
+            )
+    ).applyStyle(
+            Style(
+                    flex = Flex(
+                            justifyContent = JustifyContent.FLEX_END,
+                            grow = 1.0
+                    ),
+                    margin = EdgeValue(horizontal = 8.unitReal(), bottom = 16.unitReal())
+            )
+    )
+
+    private fun createBottomButton(text: String, remoteIconUrl: String) = Touchable(
+            child = Container(
+                    children = listOf(
+                            Image(
+                                    path = ImagePath.Remote(remoteUrl = remoteIconUrl)
+                            ).applyStyle(
+                                    Style(
+                                            size = Size(width = 40.unitReal(), height = 40.unitReal())
+                                    )
+                            ).applyFlex(
+                                    Flex(
+                                            alignSelf = AlignSelf.CENTER
+                                    )
+                            ),
+                            Text(
+                                    text = text,
+                                    alignment = TextAlignment.CENTER,
+                                    styleId = "BottomNavigationText"
+                            ).applyFlex(
+                                    Flex(
+                                            alignSelf = AlignSelf.CENTER
+                                    )
+                            )
+                    )
+            ).applyFlex(
+                    Flex(
+                            flexDirection = FlexDirection.COLUMN,
+                            flex = 1.0
+                    )
+            ).applyStyle(
+                    Style(
+                            margin = EdgeValue(all = 8.unitReal())
+                    )
+            ),
+            onPress = listOf(ToastAction(text))
     )
 }
--- src/main/java/com/vt/beaglebff/components/widgets/ItemRowDivider.kt
+++ src/main/java/com/vt/beaglebff/components/widgets/ItemRowDivider.kt
@@ -5,4 +5,11 @@ import br.com.zup.beagle.widget.Widget
 import br.com.zup.beagle.widget.context.Bind
 
 @RegisterWidget
-class ItemRowDivider(val dividerColor: Bind<String>, val dividerHeight: Bind<Int>): Widget()
+class ItemRowDivider(
+        val dividerColor: Bind<String>,
+        val dividerHeight: Bind<Int>,
+        private val leftMargin: Int? = null,
+        private val topMargin: Int? = null,
+        private val rightMargin: Int? = null,
+        private val bottomMargin: Int? = null
+): Widget()
